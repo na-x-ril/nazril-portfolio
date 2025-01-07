e4628c6cd16b64184826969f6692919b6d3dd2d8
na-x-ril <nazrilpro10@gmail.com>
2025-01-08 00:27:13 +0700

    Update project

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router basename="/">
+  <Router basename="/nazril-portfolio">
     <Routes>
       <App />
     </Routes>
