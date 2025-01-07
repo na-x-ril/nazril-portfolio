55038e01cf7391e54687bcd70049ffa85fdf6496
na-x-ril <nazrilpro10@gmail.com>
2025-01-08 00:02:14 +0700

    Update project

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nazril-portfolio/", // Ganti "nama-repo" dengan nama repository GitHub Anda
+  base: "/nazril-portfolio/",
 });
