6cdd07c6d293a7ba735a276876dafd99cbb09ccc
na-x-ril <138718203+na-x-ril@users.noreply.github.com>
2025-02-08 01:20:19 +0700

    Update Projects Page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,7 +11,7 @@ const projects = [
     description: "Portfolio website showcasing my projects, contacts and social media.",
     tags: ["Next.js", "Tailwind CSS"],
     githubUrl: "https://github.com/na-x-ril/nazril-portfolio-2",
-    liveUrl: "https://github.com/na-x-ril/nazril-portfolio-2"
+    liveUrl: "https://na-x-ril.github.io/nazril-portfolio-2/"
   },
   {
     title: "Web Injector",
