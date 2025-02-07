20f68220ec3d6cf6bd9d71b57ce14954ac1bfbfb
na-x-ril <nazrilpro10@gmail.com>
2025-02-08 01:43:35 +0700

    Update metadata

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: "/nazril-portfolio-2",
   assetPrefix: "/nazril-portfolio-2/",
   eslint: {
     ignoreDuringBuilds: true,
